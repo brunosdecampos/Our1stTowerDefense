501448dede973f06d7d0ba73d3596a080528b883
Sergio Monroy Escalante <smonroy@MacBook-Pro-de-Sergio.local>
2017-04-04 15:56:43 -0400

    Instantiate enemies version 1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public float enemyCreationTime = 3;
+	float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	//	Instantiate(enemyPrefab, new Vector3 (-10, 0.5f, -8), Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timer)
+		{
+			Instantiate (enemyPrefab, new Vector3 (-17, 0.5f, -8), Quaternion.identity);
+			timer = Time.time + enemyCreationTime;
+		}
+	}
+}
