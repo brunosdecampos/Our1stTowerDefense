47f0380ed8b93f2ab12658dd90c29b4dace8a759
wes-angus <wesman93@gmail.com>
2017-04-04 10:56:33 -0400

    Path checking works
    Tower turns red if overlapping with the path, and cannot be placed
    unless it isn't over the path anymore.

--- Assets/Scripts/TowerMove.cs
+++ Assets/Scripts/TowerMove.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class TowerMove : MonoBehaviour
 {
     TowerShoot ts;
+    bool placable = true;
+    Color oldColor;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         ts = transform.GetChild(0).GetComponent<TowerShoot>();
-	}
+        oldColor = GetComponent<Renderer>().material.color;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -22,22 +25,41 @@ public class TowerMove : MonoBehaviour
             pos.y = 0.5f;
             transform.position = pos;
             Debug.Log(pos);
-            if(Input.GetMouseButtonDown(0))
+            if (placable)
             {
-                //Set materials to opaque
-                Color oldColor = GetComponent<Renderer>().material.color;
-                GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
-                transform.GetChild(0).GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
-                transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //Set materials to opaque
+                    GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
+                    transform.GetChild(0).GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
+                    transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1);
 
-                //Enable colliders
-                GetComponent<Collider>().enabled = true;
-                transform.GetChild(0).GetComponent<Collider>().enabled = true;
-                transform.GetChild(1).GetComponent<Collider>().enabled = true;
-                transform.GetChild(0).GetChild(0).GetComponent<Collider>().enabled = true;
+                    //Enable colliders
+                    GetComponent<Collider>().isTrigger = false;
+                    transform.GetChild(0).GetComponent<Collider>().enabled = true;
+                    transform.GetChild(1).GetComponent<Collider>().enabled = true;
+                    transform.GetChild(0).GetChild(0).GetComponent<Collider>().enabled = true;
 
-                ts.placed = true;
+                    ts.placed = true;
+                }
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("PathBox"))
+        {
+            placable = false;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("PathBox"))
+        {
+            placable = true;
+            GetComponent<Renderer>().material.color = oldColor;
         }
     }
 }
