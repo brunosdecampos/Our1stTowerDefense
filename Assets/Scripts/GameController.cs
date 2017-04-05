7ad3c1b12bfc2d15c2c90f477954f00539f8bdff
Sergio Monroy Escalante <smonroy@MacBook-Pro-de-Sergio.local>
2017-04-05 15:07:37 -0400

    Enemies left label

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public List<GameObject> enemyPrefab;
-	public float enemyCreationTime = 3;
+	public float enemyCreationTime = 1;
+	public float enemyDelayTime = 4;
 	public string enemyOrder;
 	float timer = 0;
 	int enemyIndex; // next enemy
+	int enemyTotal;
     public Text winText;
     EnemyProperties castle_ep;
     Score score;
     MoneyManager mm;
+	Text enemyLeftText;
+	string[] enemyList;
+	bool end = false;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +26,12 @@ public class GameController : MonoBehaviour {
         winText = transform.GetChild(0).GetChild(2).GetComponent<Text>();
         winText.text = "";
         castle_ep = GameObject.FindGameObjectWithTag("Castle").GetComponent<EnemyProperties>();
+		enemyTotal = enemyOrder.Replace(",","").Replace("0","").Length;
+
+		enemyLeftText = transform.GetChild(0).GetChild(4).GetComponent<Text>();
+		enemyLeftText.text = "Enemies left: " + enemyTotal.ToString ();
+
+		enemyList = enemyOrder.Split (',');
         score = GetComponent<Score>();
         mm = GetComponent<MoneyManager>();
     }
@@ -29,7 +40,7 @@ public class GameController : MonoBehaviour {
 	void Update () {
 		int enemyType;
 
-		if (enemyIndex < enemyOrder.Split (',').GetLength(0)) {	// if the enemyOrder is not finished
+		if (enemyIndex < enemyList.Length) {	// if the enemyOrder is not finished
 			if (Time.time > timer) {
 				Debug.Log("index: "+enemyIndex.ToString()+": "+ enemyOrder.Split(',')[enemyIndex]);
 				enemyType = System.Convert.ToInt32 (enemyOrder.Split(',')[enemyIndex]);
@@ -40,18 +51,21 @@ public class GameController : MonoBehaviour {
 
 				if (enemyType > 0) { // is there an enemy in turn?
 					Instantiate (enemyPrefab [enemyType - 1], new Vector3 (-17, 0.5f, -8), Quaternion.identity);
+					enemyTotal--;
+					enemyLeftText.text = "Enemies left: " + enemyTotal.ToString ();
 				} else {
-					timer += enemyCreationTime * 4;		// extra time with enemyType = 0
+					timer += enemyDelayTime;		
 				}
 
 			}
 		}
-        else
+		else if (!end)
         {
             if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
             {
                 if (castle_ep.GetHealth() > 0)
                 {
+					end = true;
                     winText.text = "YOU WIN!";
                     //TODO: total score here
                     score.scoreNum += score.levelClearBonus;
