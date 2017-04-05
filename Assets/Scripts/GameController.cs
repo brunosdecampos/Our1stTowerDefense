aa8bf489765e410dc41a9478329b1844e8e1ff9d
Sergio Monroy Escalante <smonroy@MacBook-Pro-de-Sergio.local>
2017-04-05 01:31:18 -0400

    4 kind of enemies and first sequence of them

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,35 @@ public class GameController : MonoBehaviour {
 
 	public List<GameObject> enemyPrefab;
 	public float enemyCreationTime = 3;
+	public string enemyOrder;
 	float timer = 0;
+	int enemyIndex; // next enemy
 
 	// Use this for initialization
 	void Start () {
-	//	Instantiate(enemyPrefab, new Vector3 (-10, 0.5f, -8), Quaternion.identity);
+		enemyIndex = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > timer)
-		{
-			Instantiate (enemyPrefab[0], new Vector3 (-17, 0.5f, -8), Quaternion.identity);
-			timer = Time.time + enemyCreationTime;
+		int enemyType;
+
+		if (enemyIndex < enemyOrder.Split (',').GetLength(0)) {	// if the enemyOrder is not finished
+			if (Time.time > timer) {
+				Debug.Log("index: "+enemyIndex.ToString()+": "+(string)enemyOrder.Split(',')[enemyIndex]);
+				enemyType = System.Convert.ToInt32 ((string)enemyOrder.Split (',')[enemyIndex]);
+				enemyIndex++;
+//				Debug.Log (enemyType.ToString ());
+
+				timer = Time.time + enemyCreationTime;
+
+				if (enemyType > 0) { // is there an enemy in turn?
+					Instantiate (enemyPrefab [enemyType - 1], new Vector3 (-17, 0.5f, -8), Quaternion.identity);
+				} else {
+					timer += enemyCreationTime * 4;		// extra time with enemyType = 0
+				}
+
+			}
 		}
 	}
 }
