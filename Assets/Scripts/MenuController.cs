2ca84605f7157c511b5b9f0b8d82ea402ea21f7b
Bruno Campos <brunosiqueiradecampos@gmail.com>
2017-04-12 17:08:22 -0400

    Menu and Enemy
    Menu done and enemy animated

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,20 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-	public GameObject optionsContainer;
+	private GameObject optionsContainer;
+	public Text submenuTitle;
+	public Text instructions;
+	private int initialXPosition = Screen.width, finalXPosition = 600, tutorialBtnCounter = 0, optionsBtnCounter = 0, submenuContainerCounter = 0;
+	private bool tutorialBtnClicked, optionsBtnClicked;
 
 	void Start ()
-    {
+	{
 		optionsContainer = GameObject.FindGameObjectWithTag ("OptionsContainer");
-		// optionsContainer.SetActive (true); // optionsContainer.enabled = false;
+		optionsContainer.transform.position += new Vector3 (initialXPosition, 0, 0);
+	}
+
+	void Update ()
+	{
+		if (tutorialBtnClicked) {
+			if (tutorialBtnCounter % 2 != 0) {
+				ShowSubmenuContainer ();
+			} else {
+				HideSubmenuContainer ();
+			}
+		}
+
+		if (optionsBtnClicked) {
+			if (optionsBtnCounter % 2 != 0) {
+				ShowSubmenuContainer ();
+			} else {
+				HideSubmenuContainer ();
+			}
+		}
+
 	}
 
 	public void PlayGame ()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("LevelSelector");
+	}
+
+	public void ClickTutorialButton ()
+	{
+		instructions.enabled = true;
+		submenuTitle.text = "Tutorial";
+		optionsBtnCounter = 0;
+		tutorialBtnCounter++;
+		optionsBtnClicked = false;
+		tutorialBtnClicked = true;
+	}
+
+	public void ClickOptionsButton ()
+	{
+		instructions.enabled = false;
+		submenuTitle.text = "Options";
+		tutorialBtnCounter = 0;
+		optionsBtnCounter++;
+		tutorialBtnClicked = false;
+		optionsBtnClicked = true;
+	}
+
+	public void ShowSubmenuContainer ()
+	{
+		// Stop moving when reaching the maximum x position
+		if (optionsContainer.transform.position.x > finalXPosition) {
+			optionsContainer.transform.position += new Vector3 (-50, 0, 0);
+		}
+	}
+
+	public void HideSubmenuContainer ()
+	{
+		// Stop moving when reaching the initial x position
+		if (optionsContainer.transform.position.x < initialXPosition) {
+			optionsContainer.transform.position += new Vector3 (50, 0, 0);
+		}
 	}
 
 	public void QuitGame ()
