43597ecd67aa13f50315765ea3a935b234158b5f
wes-angus <wesman93@gmail.com>
2017-04-17 22:50:16 -0400

    Enemy faces direction of movement

---
+++ Assets/Scripts/OrientToMoveDir.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientToMoveDir : MonoBehaviour
+{
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rb.velocity.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
+        }
+	}
+}
