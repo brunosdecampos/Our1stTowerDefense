35df8f09046dec5e6290e5f7eb0fea85ade7e0a1
Bruno Campos <brunosiqueiradecampos@gmail.com>
2017-04-18 19:51:14 -0400

    Options Menu and Sound Effects

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,10 +7,10 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
 	private GameObject optionsContainer;
-	public Text submenuTitle;
-	public Text instructions;
-	private int initialXPosition = Screen.width, finalXPosition = 600, tutorialBtnCounter = 0, optionsBtnCounter = 0, submenuContainerCounter = 0;
+	public Text submenuTitle, instructions, soundsOnOff, effectsOnOff;
+	private int initialXPosition = Screen.width, finalXPosition = 600, tutorialBtnCounter = 0, optionsBtnCounter = 0, submenuContainerCounter = 0, soundsBtnCounter = 0, effectsBtnCounter = 0;
 	private bool tutorialBtnClicked, optionsBtnClicked;
+	public AudioSource song;
 
 	void Start ()
 	{
@@ -47,6 +47,8 @@ public class MenuController : MonoBehaviour
 	{
 		instructions.enabled = true;
 		submenuTitle.text = "Tutorial";
+		soundsOnOff.GetComponentInChildren<CanvasRenderer>().SetAlpha(0);
+		effectsOnOff.GetComponentInChildren<CanvasRenderer>().SetAlpha(0);
 		optionsBtnCounter = 0;
 		tutorialBtnCounter++;
 		optionsBtnClicked = false;
@@ -57,6 +59,8 @@ public class MenuController : MonoBehaviour
 	{
 		instructions.enabled = false;
 		submenuTitle.text = "Options";
+		soundsOnOff.GetComponentInChildren<CanvasRenderer>().SetAlpha(1);
+		effectsOnOff.GetComponentInChildren<CanvasRenderer>().SetAlpha(1);
 		tutorialBtnCounter = 0;
 		optionsBtnCounter++;
 		tutorialBtnClicked = false;
@@ -76,6 +80,32 @@ public class MenuController : MonoBehaviour
 		// Stop moving when reaching the initial x position
 		if (optionsContainer.transform.position.x < initialXPosition) {
 			optionsContainer.transform.position += new Vector3 (50, 0, 0);
+		}
+	}
+
+	public void ClickSoundsButton ()
+	{
+		soundsBtnCounter++;
+
+		if (soundsBtnCounter % 2 != 0) {
+			song.Stop ();
+			soundsOnOff.text = "Sounds: Off";
+		} else {
+			song.Play ();
+			soundsOnOff.text = "Sounds: On";
+		}
+	}
+
+	public void ClickEffectsButton ()
+	{
+		effectsBtnCounter++;
+
+		if (effectsBtnCounter % 2 != 0) {
+			// soundEffect.volume = 0;
+			effectsOnOff.text = "Effects: Off";
+		} else {
+			// soundEffect.volume = 1;
+			effectsOnOff.text = "Effects: On";
 		}
 	}
 
